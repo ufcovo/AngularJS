29cef3b5509195bf183989bc4e9c9d30b41c216f
Mert <79603812+ufcovo@users.noreply.github.com>
2022-07-17 12:19:08 +0300

    AngularJS
    Lesson for AngularJS

---
+++ AngularJs/AngularJs/controller.js
@@ -0,0 +1,8 @@
+app.controller("myControl", function ($scope) {
+    $scope.name = "Mert";
+    $scope.surname = "Bulut";
+});
+
+app.controller("secondControl", function ($scope) {
+    $scope.age = 30;
+});
